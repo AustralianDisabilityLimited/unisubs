e8ca9ee7c28d2ae895d9082d7a51546a08c7aeee
Steve Losh <steve@stevelosh.com>
2011-09-20 17:54:44 -0400

    Serve /user-data/ through Django when in DEBUG mode.

--- urls.py
+++ urls.py
@@ -132,6 +132,8 @@ if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
          {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
+        (r'^user-data/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
         (r'raw_template/(?P<template>.*)', 'django.views.generic.simple.direct_to_template'),
     )
 

51cbc5aa85329a0e85804d5fa498a0082e0d001b
alerion <alerion.um@gmail.com>
2011-10-13 09:29:59 +0300

    add Armenian language, https://unisubs.sifterapp.com/projects/12298/issues/448264/comments

--- settings.py
+++ settings.py
@@ -113,6 +113,7 @@ ALL_LANGUAGES['lkt'] = gettext_noop(u'Lakota')
 ALL_LANGUAGES['kw'] = gettext_noop(u'Cornish')
 ALL_LANGUAGES['tlh'] = gettext_noop(u'Klingon')
 ALL_LANGUAGES['mt'] = gettext_noop(u'Maltese')
+ALL_LANGUAGES['hy'] = gettext_noop(u'Armenian')
 
 del ALL_LANGUAGES['no']
 ALL_LANGUAGES = tuple(i for i in ALL_LANGUAGES.items())

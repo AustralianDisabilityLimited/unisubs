56aa039ec756381fa36c892173107ce723bf3382
Craig Zheng <craig.zheng@gmail.com>
2011-10-31 18:35:23 -0400

    Markup doesn't belong in here...

--- apps/videos/forms.py
+++ apps/videos/forms.py
@@ -386,11 +386,11 @@ class AddFromFeedForm(forms.Form, AjaxForm):
     VIDEOS_LIMIT = 10
     
     youtube_feed_url_pattern =  'https://gdata.youtube.com/feeds/api/users/%s/uploads'
-    usernames = UsernameListField(required=False, label=_(u'Youtube usernames'), help_text=_(u'<span class="hint">Enter usernames separated by comma.</span>'))
+    usernames = UsernameListField(required=False, label=_(u'Youtube usernames'), help_text=_(u'Enter usernames separated by comma.'))
     youtube_user_url = StripRegexField(youtube_user_url_re, required=False, label=_(u'Youtube page link.'), 
-                                       help_text=_(u'<span class="hint">For example: http://www.youtube.com/user/username</span>'))
-    feed_url = FeedURLField(required=False, help_text=_(u'<span class="hint">Supported: Youtube, Vimeo, Blip or Dailymotion. Only supported sites added.</span>'))
-    save_feed = forms.BooleanField(required=False, label=_(u'Save feed'), help_text=_(u'<span class="hint checkbox_hint">Choose this if you wish to add videos from this feed in the future. Only valid RSS feeds will be saved.</span>'))
+                                       help_text=_(u'For example: http://www.youtube.com/user/username'))
+    feed_url = FeedURLField(required=False, help_text=_(u'Supported: Youtube, Vimeo, Blip or Dailymotion. Only supported sites added.'))
+    save_feed = forms.BooleanField(required=False, label=_(u'Save feed'), help_text=_(u'Choose this if you wish to add videos from this feed in the future. Only valid RSS feeds will be saved.'))
     
     def __init__(self, user, *args, **kwargs):
         if not user.is_authenticated():

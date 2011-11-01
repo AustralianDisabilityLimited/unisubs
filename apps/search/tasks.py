7836dbc0a1e6f6d85bbb95bddf8adb3dd918cbe5
Arthur Debert <arthur@stimuli.com.br>
2011-11-01 16:31:26 -0200

    Made solr indexing starting time configurable to avoid having both staging and prod doing it at the same time

--- apps/search/tasks.py
+++ apps/search/tasks.py
@@ -22,6 +22,10 @@ from celery.schedules import crontab
 from celery.decorators import periodic_task
 from django.core.management import call_command
 
-@periodic_task(run_every=crontab(minute=0, hour=0))
+from django.conf import settings
+
+SEARCH_INDEXING_HOUR_STARTS = getattr(settings, "SEARCH_INDEXING_HOUR_STARTS", 0)
+
+@periodic_task(run_every=crontab(minute=0, hour=SEARCH_INDEXING_HOUR_STARTS))
 def update_search_index():
     call_command('update_index', verbosity=2)
